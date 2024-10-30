a204dea9962ca5c4bc7a3f323ebffa920c56d11b
Gil Vargas <gil@neros.tech>
2024-10-29 17:13:06 -0700

    Microchip version

---
+++ VRX_Diversity/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 10/24/2024 4:08:29 PM
+ * Author : gil
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
